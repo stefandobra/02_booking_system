edc1b3ffe6c32427359a1c160e3e2b3caafbd0ba
Stefan Dobra <dobrastefan@gmail.com>
2026-04-16 17:45:22 +0100

    Add search and update client functions, move save/load to client_service.py, update imports and menu in booking_system.py

--- booking_system.py
+++ booking_system.py
@@ -1,6 +1,6 @@
 from client import Client
-from client_service import add_client
-import json
+from client_service import add_client, save_clients, load_clients, update_client, search_client, view_clients
+
 
 clients = []
 
@@ -8,40 +8,11 @@ def display_menu():
     print(f"\n--- Booking System ---")
     print("1. View clients")
     print("2. Add client")
-    print("3. Update client")
-    print("4. Delete client")
+    print("3. Search client")
+    print("4. Update client")
+    print("5. Delete client")
     print("0. Exit")
 
-def save_clients(clients):
-    clients_dict=[]
-    for client in clients:
-        clients_dict.append(client.__dict__)
-              
-    data_to_save = {
-        "clients": clients_dict
-    }
-    
-    with open ("data.json", "w") as file:
-        json.dump(data_to_save, file)
-
-def load_clients():
-    try:
-        clients_from_file = []
-        with open("data.json", "r") as file:
-            data_to_load = json.load(file)
-        
-        for client in data_to_load["clients"]:
-            clients_from_file.append(Client(**client))
-        return clients_from_file
-
-    except FileNotFoundError:
-        print("No saved data found, starting fresh")
-        
-        return clients
-    
-    except json.JSONDecodeError as error:
-        print("Invalid JSON syntax:", error)
-        return clients
         
 clients = load_clients()
 
@@ -51,14 +22,16 @@ while True:
     option = input("Please select an option: ")
 
     if option == "1":
-        continue
+        view_clients(clients)
     elif option == "2":
         new_client = add_client()
         clients.append(new_client)
         print(f"\nNew client ({new_client.first_name} {new_client.last_name}) added succesfully!")
     elif option == "3":
-        continue
+        search_client(clients)
     elif option == "4":
+        update_client(clients)
+    elif option == "5":
         continue
     elif option == "0":
         print("Have a good day!")
--- client_service.py
+++ client_service.py
@@ -1,5 +1,36 @@
 import datetime
 from client import Client
+import json
+
+def save_clients(clients):
+    clients_dict=[]
+    for client in clients:
+        clients_dict.append(client.__dict__)
+              
+    data_to_save = {
+        "clients": clients_dict
+    }
+    
+    with open ("data.json", "w") as file:
+        json.dump(data_to_save, file)
+
+def load_clients():
+    try:
+        clients_from_file = []
+        with open("data.json", "r") as file:
+            data_to_load = json.load(file)
+        
+        for client in data_to_load["clients"]:
+            clients_from_file.append(Client(**client))
+        return clients_from_file
+
+    except FileNotFoundError:
+        print("No saved data found, starting fresh")
+        return []
+    
+    except json.JSONDecodeError as error:
+        print("Invalid JSON syntax:", error)
+        return []
 
 def add_client():
     print("\n--- Please enter new client details ---")
@@ -92,3 +123,96 @@ def view_clients(clients: list):
             i += 1
     else:
         print("No clients saved!")
+
+def search_client(clients: list):
+    print("\n--- Search Client ---")
+    print("1. Search by email")
+    print("2. Search by phone")
+
+    while True: 
+        option = input("Please select an option: ").strip()
+        if option in ["1", "2"]:
+            break  
+        else:
+            print("Invalid selection Please enter '1' or '2'")
+
+    if option == "1":
+        search_for = input("Please enter email address: ")
+    else:
+        search_for = input("Please enter phonu number: ")
+    
+    if clients:
+        for client in clients:
+            if option == "1":
+                if client.email_address == search_for:
+                    print(f"{client.first_name} {client.last_name} p: {client.phone_number} e: {client.email_address}")
+                    return client
+            else:
+                if client.phone_number == search_for:
+                    print(f"{client.first_name} {client.last_name} p: {client.phone_number} e: {client.email_address}")
+                    return client
+        print(f"No client found for {search_for}")
+    else:
+        print("No clients saved")
+
+def update_client(clients: list):
+    client_to_update = search_client(clients)
+
+    if client_to_update:
+        print(f"--- Update Client {client_to_update.first_name} {client_to_update.last_name} ---")
+        print("What do you want to update for this client?")
+        print(f"1. First name\n2. Last name\n3. Phone number\n4. Email address\n5. Notes\n6. Gender\n7. Date of birth\n8. Source\n9. Cancel")
+
+        while True:
+            option = input("Select option: ").strip()
+            if option in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+                break
+            else:
+                print("Invalid selection. Plese enter 1-9")
+            
+        if option == "1":
+            new_first_name = input("Enter new first name: ")
+            client_to_update.first_name = new_first_name
+        elif option == "2":
+            new_last_name = input("Enter new last name: ")
+            client_to_update.last_name = new_last_name
+        elif option == "3":
+            new_phone_number = input("Enter new phone number: ")
+            client_to_update.phone_number = new_phone_number
+        elif option == "4":
+            new_email_address = input("Enter new email address: ")
+            client_to_update.email_address = new_email_address
+        elif option == "5":
+            return
+        elif option == "6":
+            new_gender = input("Enter new gender: ")
+            client_to_update.gender = new_gender
+        elif option == "7":
+            new_dob = input("Enter new date of birth: ")
+            client_to_update.dob = new_dob
+        elif option == "8":
+            new_source = input("Enter new source: ")
+            client_to_update.source = new_source
+        elif option == "9":
+            print("Client update cancelled")
+            return
+        print(f"--- Client {client_to_update.first_name} {client_to_update.last_name} updated---")
+        save_clients(clients)
+        
+    else:
+        return
+    
+
+    
+
+
+
+
+
+
+
+    
+
+    
+
+
